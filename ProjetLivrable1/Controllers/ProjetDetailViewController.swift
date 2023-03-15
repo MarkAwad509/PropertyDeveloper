7a1715782bb4a0e1fd8d0691caceb5c002a0a729
MarkAwad509 <markawad509@gmail.com>
2023-03-15 09:02:14 -0400

    bringing project detail view over from dev/UI branch

---
+++ ProjetLivrable1/Controllers/ProjetDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProjetDetailViewController.swift
+//  ProjetLivrable1
+//
+//  Created by Mark Awad on 2023-03-15.
+//
+
+import UIKit
+
+class ProjetDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
